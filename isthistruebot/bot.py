2e5f33c9c21f811fa1c2696df9e3ba1366c2169b
localadmin <briz.andrew@gmail.com>
2018-02-21 13:09:26 -0500

    Adding daemonizing.

--- isthistruebot/bot.py
+++ isthistruebot/bot.py
@@ -1,18 +1,37 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
 import sys
+import time
 import tweepy
+
 from tweeter import twitter, StreamListener
+from daemon import Daemon
 
 
-# Dealing With Unicode Stuff for scraping
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-if __name__ == '__main__':
-    """
-    Create a Custom Tweepy stream listener and start listening for Tweets tagged with @IsThisTrueBot
-    """
-    stream = tweepy.Stream(auth=twitter.auth, listener=StreamListener())
-    stream.filter(track=['@IsThisTrueBot'], async=True)
+class BotDaemon(Daemon):
+    def run(self):
+        """
+        Create a Custom Tweepy stream listener and start listening for Tweets tagged with @IsThisTrueBot
+        """
+        stream = tweepy.Stream(auth=twitter.auth, listener=StreamListener())
+        stream.filter(track=['@IsThisTrueBot'], async=True)
+
+
+if __name__ == "__main__":
+    daemon = BotDaemon('/tmp/daemon-example.pid')
+    if len(sys.argv) == 2:
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
